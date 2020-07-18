f0ea771b18639d6b438ccf5f1a3495eaa975a8d2
lsy26499 <lsy26499@gmail.com>
2020-07-19 02:53:47 +0900

    :wastebasket: Remove some codes
    - 코드 클린업을 진행했습니다.

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
