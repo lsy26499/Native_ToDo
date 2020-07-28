09e6430cd480d4ae29b8bcea3f6d3545a6a8c6cf
lsy26499 <lsy26499@gmail.com>
2020-07-28 18:53:19 +0900

    :sparkles: Add Firebase Social login
    - Firebase 구글 로그인을 구현했습니다.

--- pages/Setting.js
+++ pages/Setting.js
@@ -1,61 +1,80 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import AsyncStorage from '@react-native-community/async-storage';
+import * as GoogleSignIn from 'expo-google-sign-in';
 
 const Setting = () => {
-	const [isLogin, setIsLogin] = useState(false);
+    const [isLogin, setIsLogin] = useState(false);
+    const [user, setUser] = useState(null);
 
-	const storeData = async () => {
-		try {
-			setIsLogin((prevState) => !prevState);
-			await AsyncStorage.setItem('isLogin', JSON.stringify(isLogin));
-		} catch (error) {
-			console.log(error);
-		}
-	};
+    useEffect(() => {
+        initAsync();
+    }, []);
 
-	const getData = async () => {
-		const data = await AsyncStorage.getItem('isLogin');
-		setIsLogin(data);
-	};
+    const initAsync = async () => {
+        await GoogleSignIn.initAsync({
+            clientId:
+                '652299888580-s33d4gmd23bhufq90ftcgiu9icjpump3.apps.googleusercontent.com',
+        });
+        _syncUserWithStateAsync();
+    };
 
-	useEffect(() => {
-		getData();
-	}, []);
+    const _syncUserWithStateAsync = async () => {
+        const user = await GoogleSignIn.signInSilentlyAsync();
+        setUser(user);
+    };
 
-	return (
-		<View style={styles.container}>
-			<TouchableOpacity
-				style={styles.loginButton}
-				onPress={() => {
-					storeData();
-				}}
-			>
-				<Text style={styles.defaultFontSetting}>
-					{isLogin ? '로그아웃' : '로그인'}
-				</Text>
-			</TouchableOpacity>
-			<Text style={styles.defaultFontSetting}>
-				{isLogin ? '로그인중입니다' : '로그아웃되었습니다'}
-			</Text>
-		</View>
-	);
+    const signOutAsync = async () => {
+        await GoogleSignIn.signOutAsync();
+        setUser(null);
+    };
+
+    const signInAsync = async () => {
+        try {
+            await GoogleSignIn.askForPlayServicesAsync();
+            const { type, user } = await GoogleSignIn.signInAsync();
+            if (type === 'success') {
+                _syncUserWithStateAsync();
+            }
+        } catch ({ message }) {
+            console.log(message);
+            alert('login: Error:' + message);
+        }
+    };
+
+    const onPress = () => {
+        if (user) {
+            signOutAsync();
+        } else {
+            signInAsync();
+        }
+    };
+
+    return (
+        <View style={styles.container}>
+            <TouchableOpacity style={styles.loginButton} onPress={onPress}>
+                <Text style={styles.defaultFontSetting}>구글 로그인</Text>
+            </TouchableOpacity>
+            <Text style={styles.defaultFontSetting}>
+                {isLogin ? '로그인중입니다' : '로그아웃되었습니다'}
+            </Text>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-	defaultFontSetting: {
-		fontFamily: '',
-		fontSize: 30,
-		fontWeight: 'bold',
-	},
-	container: {
-		flex: 1,
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-	loginButton: {
-		paddingBottom: 20,
-	},
+    defaultFontSetting: {
+        fontFamily: '',
+        fontSize: 30,
+        fontWeight: 'bold',
+    },
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    loginButton: {
+        paddingBottom: 20,
+    },
 });
 
 export default Setting;
