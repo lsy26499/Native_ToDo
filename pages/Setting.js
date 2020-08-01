167d8c4238d2eaba580c2e51d4336be398ba9d85
lsy26499 <lsy26499@gmail.com>
2020-08-01 22:23:34 +0900

    :sparkles: Add Facebook SignIn

--- pages/Setting.js
+++ pages/Setting.js
@@ -1,61 +1,63 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import AsyncStorage from '@react-native-community/async-storage';
+import * as Facebook from 'expo-facebook';
 
 const Setting = () => {
-	const [isLogin, setIsLogin] = useState(false);
+    const [isLogin, setIsLogin] = useState(false);
 
-	const storeData = async () => {
-		try {
-			setIsLogin((prevState) => !prevState);
-			await AsyncStorage.setItem('isLogin', JSON.stringify(isLogin));
-		} catch (error) {
-			console.log(error);
-		}
-	};
+    const logIn = async () => {
+        try {
+            await Facebook.initializeAsync('738535016940375');
+            const data = await Facebook.logInWithReadPermissionsAsync({
+                permissions: ['public_profile', 'email'],
+            });
 
-	const getData = async () => {
-		const data = await AsyncStorage.getItem('isLogin');
-		setIsLogin(data);
-	};
+            if (data.type === 'success') {
+                // Get the user's name using Facebook's Graph API
+                console.log(data);
+                // const response = await fetch(`https://graph.facebook.com/me?access_token=${token}`);
+                // Alert.alert('Logged in!', `Hi ${(await response.json()).name}!`);
+            } else {
+                // type === 'cancel'
+            }
+        } catch ({ message }) {
+            alert(`Facebook Login Error: ${message}`);
+        }
+    };
 
-	useEffect(() => {
-		getData();
-	}, []);
-
-	return (
-		<View style={styles.container}>
-			<TouchableOpacity
-				style={styles.loginButton}
-				onPress={() => {
-					storeData();
-				}}
-			>
-				<Text style={styles.defaultFontSetting}>
-					{isLogin ? '로그아웃' : '로그인'}
-				</Text>
-			</TouchableOpacity>
-			<Text style={styles.defaultFontSetting}>
-				{isLogin ? '로그인중입니다' : '로그아웃되었습니다'}
-			</Text>
-		</View>
-	);
+    return (
+        <View style={styles.container}>
+            <TouchableOpacity
+                style={styles.loginButton}
+                onPress={() => {
+                    logIn();
+                }}
+            >
+                <Text style={styles.defaultFontSetting}>
+                    {isLogin ? '로그아웃' : '로그인'}
+                </Text>
+            </TouchableOpacity>
+            <Text style={styles.defaultFontSetting}>
+                {isLogin ? '로그인중입니다' : '로그아웃되었습니다'}
+            </Text>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-	defaultFontSetting: {
-		fontFamily: '',
-		fontSize: 30,
-		fontWeight: 'bold',
-	},
-	container: {
-		flex: 1,
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-	loginButton: {
-		paddingBottom: 20,
-	},
+    defaultFontSetting: {
+        fontFamily: '',
+        fontSize: 30,
+        fontWeight: 'bold',
+    },
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    loginButton: {
+        paddingBottom: 20,
+    },
 });
 
 export default Setting;
